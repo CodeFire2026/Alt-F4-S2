4c2cee9553459c4dd5bdd4365a9bc65fcce7d349
Manuel Monsalve <mmonsalve1357@gmail.com>
2026-09-07 23:29:19 -0300

    Fix ej 12 comment

--- Python/Clase4/EjercicioColecciones12.py
+++ Python/Clase4/EjercicioColecciones12.py
@@ -1,4 +1,4 @@
-# Ejercicio 9: Mostrar una frase sin espacios y contar su longitud Hacer un programa donde el usuario ingrese una frase,
+# Ejercicio 12: Mostrar una frase sin espacios y contar su longitud Hacer un programa donde el usuario ingrese una frase,
 # se le devuelva la misma frase pero sin espacios en blanco, y además un contador de cuántos caracteres tiene la frase
 # (sin contar los espacios en blanco).
 # Ejemplo: frase = vivir por siempre en paz
